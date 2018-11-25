06eafc2714d7bead56615978bbe0512204d18876
nazym <nazymanteater@gmail.com>
2018-11-26 00:53:52 +0300

    modified file libft.h

--- libft.h
+++ libft.h
@@ -13,8 +13,13 @@ int	ft_toupper(int c);
 int	ft_tolower(int c);
 int	ft_atoi(const char *str);
 size_t	ft_strlen(const char *str);
-char    *ft_strcpy(char *dest, const char *src);
-char    *ft_strncpy(char *dest, const char *src, size_t n);
-char    *ft_strdup(const char *str);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
+char	*ft_strdup(const char *str);
+int	ft_strcmp(const char *s1, const char *s2);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strcat(char *dest, const char *src);
+char	*ft_strncat(char *dest, const char *src, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
