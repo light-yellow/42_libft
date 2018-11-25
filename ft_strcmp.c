1d0b81f662a98adea3f3406a921aca6e1cf4cb10
nazym <nazymanteater@gmail.com>
2018-11-25 19:42:27 +0300

    added files ft_strcmp.c

---
+++ ft_strcmp.c
@@ -0,0 +1,10 @@
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned int i;
+	
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i += 1;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+
+}
