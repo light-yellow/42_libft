5c941e2f8fac832012bea4db8925a527eb0c4b2c
nazym <nazymanteater@gmail.com>
2018-11-27 03:33:32 +0300

    added file ft_strchr.c

---
+++ ft_strchr.c
@@ -0,0 +1,15 @@
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned int i;
+
+	i = 0;
+	if (c == '\0')
+		return ((char *)s);
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((char *)&s[i]);
+		i += 1;
+	}
+	return (0);
+}
