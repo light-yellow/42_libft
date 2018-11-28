9ae3165cbcc925bb5dc264c487a89e083115535b
nazym <nazymanteater@gmail.com>
2018-11-28 08:32:43 +0300

    added file ft_memmove.c, modified file ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -4,11 +4,11 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned int 		i;
 	unsigned char		*ptr_dest;
-	unsigned char		*ptr_src;
+	const unsigned char	*ptr_src;
 
 	i = 0;
 	ptr_dest = (unsigned char *)dest;
-	ptr_src = (unsigned char *)src;
+	ptr_src = (const unsigned char *)src;
 	while ((size_t)i < n)
 	{
 		ptr_dest[i] = ptr_src[i];
