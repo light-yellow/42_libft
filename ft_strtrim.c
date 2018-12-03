d906a4d869666b844d5b08df8c3ab9d42cb3e945
nazym <nazymanteater@gmail.com>
2018-12-03 19:59:07 +0300

    added ft_strtrim.c, fixed some bugs

---
+++ ft_strtrim.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+char	*ft_strtrim(char const *s)
+{
+	char	*result;
+	size_t	start;
+	size_t	end;
+
+	result = NULL;
+	if (s)
+	{
+		start = 0;
+		while (s[start] == ' ' || s[start] == '\n' || s[start] == '\t')
+			start += 1;
+		end = ft_strlen(s);
+		if (start == end)
+                        result = ft_strnew((size_t)0);
+		else
+			end -= 1;
+			while (s[end] == ' ' || s[end] == '\n' || s[end] == '\t')
+                        	end -= 1;
+			result = ft_strsub(s, start, (size_t)(end - start + 1));
+	}
+	return (result);
+}
