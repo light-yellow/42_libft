f6e85bc09cfcaf356ee1658045ca6cb9107be019
nazym <nazymanteater@gmail.com>
2018-12-03 12:41:06 +0300

    added files ft_putnbr.c and ft_putnbr_fd.c

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int tmp_n;
+
+	if (n < 0)
+	{
+		ft_putchar('-', fd);
+		tmp_n = (unsigned int)-n;
+	}
+	else
+		tmp_n = (unsigned int)n;
+	if (tmp_n >= 10)
+		ft_putnbr_fd(tmp_n / 10, fd);
+	ft_putchar_fd((char)(tmp_n % 10) + '0', fd);
+}
