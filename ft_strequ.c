c89a643bfbd90376568379dfa6def41cc28fd830
Jonos bracken Goyette <jgoyette@e2r1p1.21-school.ru>
2018-12-09 17:12:51 +0300

    modified files ft_strequ.c and ft_strnequ.c

--- ft_strequ.c
+++ ft_strequ.c
@@ -2,7 +2,7 @@
 
 int	ft_strequ(char const *s1, char const *s2)
 {
-	if (ft_strcmp(s1, s2))
+	if (!s1 || !s2 || ft_strcmp(s1, s2))
 		return (0);
 	else
 		return (1);
--- ft_strnequ.c
+++ ft_strnequ.c
@@ -2,7 +2,7 @@
 
 int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (ft_strncmp(s1, s2, n))
+	if (!s1 || !s2 || ft_strncmp(s1, s2, n))
 		return (0);
 	else
 		return (1);
